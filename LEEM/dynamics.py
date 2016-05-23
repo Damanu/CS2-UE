cc5e8e037d1de3cb707cd15f5a418859b9aa4aeb
Damanu <e.schwarzhans@gmx.at>
2016-05-23 20:15:05 +0200

    added some dynamics, 	still have to work on the addition of a new node, the normalisation and the small pertubation at the end are missing right now

--- LEEM/dynamics.py
+++ LEEM/dynamics.py
@@ -1,7 +1,7 @@
 #usr/bin/python!
 
 #Dynamics simulation of katalytic network
-#input: initialized network, ntprint number of steps after which the network gets saved and perturbed afterwards, ntjob total number of steps executed
+#input: initialized network, ntprint, ntperturb number of steps after which the network gets saved and perturbed afterwards, ntjob total number of steps executed
 
 #------------------imports-----------------
 import sys
@@ -9,19 +9,23 @@ import numpy as np
 import networkx as nx
 
 #---------------Globals---------------------------
-
-
+tres=0.01
+smallx=0.002
 
 #-----------------inputs-----------------------
-if len(sys.argv)!=3:
-	print "Usage:\n\n	dynamics.py [ntprint] [ntjob]"
+if len(sys.argv)!=4:
+	print "Usage:\n\n	dynamics.py [ntprint] [ntjob] [ntperturb]"
 	sys.exit()
 
 ntprint=int(sys.argv[1])
 ntjob=int(sys.argv[2])
+ntperturb=int(sys.argv[3])
+
 f=open("init_parameter_out.data","r")
 data=f.read()
 (S,p)=data.split()
+S=int(S)
+p=float(p)
 f.close()
 
 #---------------Subroutines-----------------------
@@ -31,22 +35,47 @@ def timestep(G,x,C):
 	return x
 #-----------------Main program-------------------
 def main():
+	weak=[]
+
 	G=nx.read_gexf("init_out.gexf")
 	x=np.array(nx.get_node_attributes(G,"x").values())
 	C=nx.to_numpy_matrix(G)
-	print x
-	print len(C)
 	i=0
 	while i<ntjob:
 		x=timestep(G,x,C)
-		if i%ntprint==0:
-			print x
-			print sum(x)
-			map(float,x)
-			dic=dict(zip(np.arange(int(S)),x))
-			nx.set_node_attributes(G,"x",dic)			
-			fn=str("./dynamic_out/dynamic_out_nt="+str(i)+".gexf")
-			nx.write_gexf(fn)
 		i+=1
+		print "i ",i
+		if i%ntprint==0: #every time i has run ntprint steps through
+			x_=map(float,x)	#change all elements to a float
+			dic=dict(zip(map(str,np.arange(S)),x_))	#write dictionary for the attributes
+			nx.set_node_attributes(G,"x",dic) #set attributes new
+			fn=str("./dynamic_out/dynamic_out_nt="+str(i)+".gexf")
+			nx.write_gexf(G,fn) #write gexf file with network
+		if i%ntperturb==0: #every time i has run ntperturb steps through
+			x_=map(float,x) 
+			dic=dict(zip(map(str,np.arange(S)),x_))
+			nx.set_node_attributes(G,"x",dic)
+			weak=[]
+			for index,xi in dic.iteritems():
+				if xi < tres:
+					weak.append(index)
+				#	print index
+			delind=np.random.randint(len(weak))
+			print delind
+#			print G.node
+			G.remove_node(str(delind)) #remove one random node of the weak nodes
+			G.add_node(str(delind))	#add node with same name
+			dic[str(delind)]=smallx #set the relative concentration of the new node to a small value
+			j=0
+			while j<S:	#iterate over all nodes
+				if j!=delind and np.random.rand()<p: #is an other node and with certain probability?
+					G.add_edge(delind,j)	#add a connection between new node and an other one
+				if j!=delind and np.random.rand()<p: #is an other node and with certain probability?
+					G.add_edge(delind,j)	#add a connection between new node and an other one
+				j+=1
+
+				
+			
+					
 if __name__=="__main__":
 	main()
