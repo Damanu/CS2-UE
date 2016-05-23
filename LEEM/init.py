5465e12760231e928c70d2ca3a360dee653601c2
Damanu <e.schwarzhans@gmx.at>
2016-05-23 13:37:06 +0200

    added the initialisation program for the network

---
+++ LEEM/init.py
@@ -0,0 +1,30 @@
+#/usr/bin/python!
+#Initailisattion of a directed random erdoes-renyi Network with probability p to connect
+
+#Input Parameter: Number of Nodes S, connecting probability p
+
+#------------Imports---------------------------
+import sys
+import networkx as nx
+import numpy as np
+#------------Globals--------------------------
+OUTPATH="./init_out.net"
+#------------Input parameters------------------
+if len(sys.argv)!=3:
+	print "Help\nCall the program like that:\n\n	init.py [number of nodes] [connecting probability p]\n"
+	sys.exit()
+
+S=int(sys.argv[1])
+p=float(sys.argv[2])
+
+#------------Main Program----------------------
+def main():
+	G=nx.fast_gnp_random_graph(S,p,directed=True) #make a random erdoes-renyi graph with S nodes and prob p
+	ran=np.random.rand(S)	# make list with S random numbers between 0 and 1
+	ran=ran/sum(ran)	# normalise ran so that the sum over all is 1
+	x=dict(zip(np.arange(S),ran)) #create a dictionary with first parameter the index of the node and second the relative population xi
+	nx.set_node_attributes(G,"x",x)	#set the attribute of the nodes
+	nx.write_pajek(G,"init_out.net")
+
+if __name__=="__main__":
+	main() 
