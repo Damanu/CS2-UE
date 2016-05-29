906590867e3f53267346f78e733387bb71fbf0f0
Damanu <e.schwarzhans@gmx.at>
2016-05-29 16:56:04 +0200

    still having trouble with the edges, i never get a giant component

--- LEEM/dynamics.py
+++ LEEM/dynamics.py
@@ -27,15 +27,14 @@ ntperturb=int(sys.argv[3])
 
 
 #---------------Globals---------------------------
-setsize=int(S*0.2)
-print setsize
+setsize=int(S*0.1)
 smallx=1./S# small x for new node and perturbation (smaller than that)
-print smallx
 nt=0
 #---------------Subroutines-----------------------
 def timestep(G,x,C):
 	x=x+np.squeeze(np.asarray(np.dot(C,x)))-x*sum(np.squeeze(np.asarray(np.dot(C,x))))#rate equation
-#	print np.squeeze(np.asarray(np.dot(C,x)))-x*sum(np.squeeze(np.asarray(np.dot(C,x))))
+	if ntperturb==0:
+		print sum(np.squeeze(np.asarray(np.dot(C,x)))-x*sum(np.squeeze(np.asarray(np.dot(C,x)))))
 	i=0
 	while i<S:
 		if x[i]<0:
@@ -62,29 +61,35 @@ def kill_node(G,x):
 	while len(weak)<setsize: #till set is full
 		minx=min(dic.values())
 		ind=dic.values().index(minx)
-		weak.append(minx) #append the smallest x
+		weak.append(int(dic.keys()[ind])) #append the smallest x key
 #		print dic.values().index(min(dic.values()))
 		del dic[dic.keys()[ind]] #delete the entry of the smallest x in the dict
-	index=str(np.random.randint(len(weak)))
+	index=str(np.random.randint(min(weak),max(weak)+1))
+	x_[int(index)]=smallx #change x of new species
 	j=0
 	while j<S:
-		if G.has_edge(str(index),str(j)):
+		if G.has_edge(index,str(j)):
 			G.remove_edge(index,str(j))	#remove a connection between new node and an other one
 		if G.has_edge(str(j),index):
 			G.remove_edge(str(j),index)	#remove a connection between new node and an other one
 		j+=1
 	j=0	
 	while j<S:	#iterate over all nodes, set new edges of new species
-		if j!=index and np.random.rand()<p: #is an other node and with certain probability?
-			G.add_edge(str(index),str(j))	#add a connection between new node and an other one
-		if j!=index and np.random.rand()<p: #is an other node and with certain probability?
-			G.add_edge(str(j),str(index))	#add a connection between new node and an other one
+		if j!=int(index) and np.random.rand()<p: #is an other node and with certain probability?
+			G.add_edge(index,str(j))	#add a connection between new node and an other one
+		if j!=int(index) and np.random.rand()<p: #is an other node and with certain probability?
+			G.add_edge(str(j),index)	#add a connection between new node and an other one
 		j+=1
 	x_=perturb_all(x_)
 	dic=dict(zip(map(str,np.arange(S)),x_))
 	nx.set_node_attributes(G,"x",dic)
 	return G
-
+def nonzerox(x):
+	count=0
+	for val in x:
+		if val > 0.0001:
+			count+=1
+	return count
 
 def perturb_all(x_):	#perturb all x by an amount smaller than smallx and rescale to normalization
 	i=0
@@ -111,9 +116,11 @@ def main():
 		nt+=1
 		if nt%ntprint==0: #every time i has run ntprint steps through
 			print_net(G,x,nt/ntprint)
-		if nt%ntperturb==0: #every time i has run ntperturb steps through
-			G=kill_node(G,x)
-			x=np.array(nx.get_node_attributes(G,"x").values())
+			print nt/ntprint,nonzerox(x)
+		if ntperturb!=0: 
+			if nt%ntperturb==0: #every time i has run ntperturb steps through
+				G=kill_node(G,x)
+				x=np.array(nx.get_node_attributes(G,"x").values())
 		
 if __name__=="__main__":
 	main()
