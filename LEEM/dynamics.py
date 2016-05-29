bdd02d5f7d737955d996b9640b14d24eb7dc5c0d
Damanu <e.schwarzhans@gmx.at>
2016-05-29 15:16:30 +0200

    the dynamics works now, but no new edges are produced

--- LEEM/dynamics.py
+++ LEEM/dynamics.py
@@ -8,11 +8,15 @@ import sys
 import numpy as np
 import networkx as nx
 
-#---------------Globals---------------------------
-setsize=15
-smallx=0.002# small x for new node and perturbation (smaller than that)
-nt=0
+
 #-----------------inputs-----------------------
+f=open("init_parameter_out.data","r")
+data=f.read()
+(S,p)=data.split()
+S=int(S)
+p=float(p)
+f.close()
+
 if len(sys.argv)!=4:
 	print "Usage:\n\n	dynamics.py [ntprint] [ntjob] [ntperturb]"
 	sys.exit()
@@ -21,16 +25,17 @@ ntprint=int(sys.argv[1])
 ntjob=int(sys.argv[2])
 ntperturb=int(sys.argv[3])
 
-f=open("init_parameter_out.data","r")
-data=f.read()
-(S,p)=data.split()
-S=int(S)
-p=float(p)
-f.close()
 
+#---------------Globals---------------------------
+setsize=int(S*0.2)
+print setsize
+smallx=1./S# small x for new node and perturbation (smaller than that)
+print smallx
+nt=0
 #---------------Subroutines-----------------------
 def timestep(G,x,C):
-	x=x+np.squeeze(np.asarray(np.dot(np.transpose(C),x)))-x*sum(np.squeeze(np.asarray(np.dot(np.transpose(C),x))))#rate equation
+	x=x+np.squeeze(np.asarray(np.dot(C,x)))-x*sum(np.squeeze(np.asarray(np.dot(C,x))))#rate equation
+#	print np.squeeze(np.asarray(np.dot(C,x)))-x*sum(np.squeeze(np.asarray(np.dot(C,x))))
 	i=0
 	while i<S:
 		if x[i]<0:
@@ -61,7 +66,6 @@ def kill_node(G,x):
 #		print dic.values().index(min(dic.values()))
 		del dic[dic.keys()[ind]] #delete the entry of the smallest x in the dict
 	index=str(np.random.randint(len(weak)))
-	print index
 	j=0
 	while j<S:
 		if G.has_edge(str(index),str(j)):
@@ -69,7 +73,6 @@ def kill_node(G,x):
 		if G.has_edge(str(j),index):
 			G.remove_edge(str(j),index)	#remove a connection between new node and an other one
 		j+=1
-	print G.edge[str(index)]
 	j=0	
 	while j<S:	#iterate over all nodes, set new edges of new species
 		if j!=index and np.random.rand()<p: #is an other node and with certain probability?
@@ -86,7 +89,7 @@ def kill_node(G,x):
 def perturb_all(x_):	#perturb all x by an amount smaller than smallx and rescale to normalization
 	i=0
 	while i<S:
-		x_[i]-=smallx*0.5
+		x_[i]+=x_[i]*(np.random.rand()-0.5)
 		if x_[i]<0:
 			x_[i]=0
 		i+=1
@@ -98,19 +101,19 @@ def main():
 	G=nx.read_gexf("init_out.gexf")
 	x=np.array(nx.get_node_attributes(G,"x").values())
 	dic=dict(zip(map(str,np.arange(S)),x))
-	C=nx.to_numpy_matrix(G)
+	nodel=map(str,np.arange(S))
+	C=nx.to_numpy_matrix(G,nodelist=nodel).transpose()
 	nt=0
 	while nt<ntjob:
-		C=nx.to_numpy_matrix(G)
-		x=np.array(nx.get_node_attributes(G,"x").values())
+		C=nx.to_numpy_matrix(G,nodelist=nodel).transpose()
+#		x=np.array(nx.get_node_attributes(G,"x").values())
 		x=timestep(G,x,C) #change the attributes according to the rate equation
 		nt+=1
 		if nt%ntprint==0: #every time i has run ntprint steps through
-			print_net(G,x,nt)
+			print_net(G,x,nt/ntprint)
 		if nt%ntperturb==0: #every time i has run ntperturb steps through
-			print nt
 			G=kill_node(G,x)
+			x=np.array(nx.get_node_attributes(G,"x").values())
 		
-	print x			
 if __name__=="__main__":
 	main()
