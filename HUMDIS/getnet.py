8fce523955ce559fb61ce1f07bf85e5fca7f2087
Damanu <e.schwarzhans@gmx.at>
2016-06-18 21:03:06 +0200

    finished the project, the two gephi files are uploaded as well

--- HUMDIS/getnet.py
+++ HUMDIS/getnet.py
@@ -18,7 +18,7 @@ def main():
 	humdis=nx.Graph()
 	humdis2=nx.Graph()
 	humgene=nx.Graph()
-
+	humbipart=nx.Graph()
 #human disease network
 	for line in data[1:]:	
 		genes_=line[2].replace(" ","").split(",") #get a list of genes in the new line
@@ -33,15 +33,13 @@ def main():
 			for x in genes_:
 				if x not in nx.get_node_attributes(humdis2,"genes")[line[0]]:#ask if one of the genes  is not in the gene pool of the disease
 					nx.get_node_attributes(humdis2,"genes")[line[0]].append(x)
-			nx.get_node_attributes(humdis2,"numofgenes")[line[0]]=len(nx.get_node_attributes(humdis2,"genes")[line[0]])
+			nx.set_node_attributes(humdis2,"numofgenes",{line[0]:float(len(nx.get_node_attributes(humdis2,"genes")[line[0]]))})
 
-			print len(nx.get_node_attributes(humdis2,"genes")[line[0]])
 		else:
-			humdis2.add_node(line[0],nid=int(line[0]),genes=genes_,Class=line[5],numofgenes=len(genes_))
+			humdis2.add_node(line[0],nid=int(line[0]),genes=genes_,Class=line[5],numofgenes=int(len(genes_)))
 	#make edges
 
 	#make edges
-	print len(humdis.nodes())
 	nodebuff=[]
 #human gene network
 	for line in data[1:]:	
@@ -50,9 +48,17 @@ def main():
 			if humgene.has_node(x):
 				nx.get_node_attributes(humgene,"disease")[x].append(line[1])
 				nx.get_node_attributes(humgene,"nid")[x].append(line[0])
+				if line[5] != nx.get_node_attributes(humgene,"Class")[x]:
+					nx.set_node_attributes(humgene,"Class",{x:0})
+					
 			else:
-				humgene.add_node(x,disease=[line[1]],nid=[line[0]])
+				humgene.add_node(x,num=float(line[0]),numofdis=float(1),disease=[line[1]],nid=[line[0]],Class=line[5])
+			nx.set_node_attributes(humgene,"numofdis",{x:float(len(nx.get_node_attributes(humgene,"disease")[x]))})
 	
+#biparted network
+	humbipart.add_nodes_from(humdis2.nodes(),bipartite=0)
+	humbipart.add_nodes_from(humgene.nodes(),bipartite=1)
+
 	for node in humgene.nodes(): #got through all genes
 		diseases=nx.get_node_attributes(humgene,"disease")[node]
 		edges=it.combinations(diseases,2)
@@ -66,7 +72,6 @@ def main():
 		edges=it.combinations(genes,2)
 		humgene.add_edges_from(edges)
 	
-	print len(humdis.nodes())
 	nx.set_node_attributes(humdis,"genes",0)
 	nx.set_node_attributes(humdis2,"genes",0)
 	nx.set_node_attributes(humgene,"nid",0)
